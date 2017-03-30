b57b125fe97e247ed97ada3ceb1a741692623a90
rozie <rozie@poczta.onet.pl>
2017-03-30 12:15:58 +0200

    introduced logging; fixed weighted mean calculation

--- abcc.py
+++ abcc.py
@@ -1,48 +1,60 @@
 #!/usr/bin/python
+# -*- coding: utf-8 -*-
 
 import argparse
+import logging
 import ping
 import yaml
 
 
+logging.basicConfig(level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+
 def get_ip_score(ip, loss_mult, lag_mult, count):
+    logger.debug("IP {} loss_mult {} lag_mult {} count {}"
+                 .format(ip, loss_mult, lag_mult, count))
+
     result = ping.quiet_ping(ip, 2, count)
     (loss, lag) = (result[0], result[2])
-#    print loss, lag, loss_mult, lag_mult
+    logger.debug("loss {} lag {}".format(loss, lag))
     if not lag:
         lag = 1000
+        logger.warning("IP {} is unreachable".format(ip))
     score = loss_mult*loss + lag_mult*lag
-    print "loss", loss, "lag", lag
+    logger.debug("Returning score {} for IP {}".format(score, ip))
     return score
 
 
 def get_route_score(route):
+    logger.debug("Getting score for route {}".format(route))
     route_sum = 0
+    weight_sum = 0
     loss_mult = route.get('loss_mult', 10)
     lag_mult = route.get('lag_mult', 1)
     for ip in route.get('IPs'):
-        weight_sum = 0
         count = route['IPs'][ip].get('count', 10)
         ip_weight = route['IPs'][ip].get('weight', 1)
         ip_score = get_ip_score(ip, loss_mult, lag_mult, count)
         route_sum += ip_score*ip_weight
         weight_sum += ip_weight
     route_score = float(route_sum/weight_sum)
-    print route_sum, route_score
+    logger.debug("Route sum is {}, route score is {}"
+                 .format(route_sum, route_score))
     return route_score
 
 
 def main():
     args = parse_arguments()
-
     try:
         with open(args.config, "r") as config:
             data = yaml.load(config)
     except:
-        print "Bug!"
+        logger.error("Couldn't read config file {}".format(config))
 
     for route in data.get('routes'):
-        print route, get_route_score(data['routes'][route])
+        logger.info("Route {} got score {}".
+                    format(route, get_route_score(data['routes'][route])))
 
 
 def parse_arguments():
